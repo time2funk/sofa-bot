66005f8b045f6f7058570e5b3a95ea54cf34fd78
sadman <m.onevegas@gmail.com>
2018-04-04 22:05:10 +0200

    xvfb

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 // const url = 'https://www.gumtree.pl/s-mieszkania-i-domy-do-wynajecia/warszawa/mieszkanie/v1c9008l3200008a1dwp1?fr=ownr&pr=1300,2300&nr=3';
 const url = 'https://www.olx.pl/nieruchomosci/mieszkania/wynajem/warszawa/?search%5Bfilter_float_price%3Afrom%5D=1000&search%5Bfilter_float_price%3Ato%5D=2200&search%5Bfilter_enum_rooms%5D%5B0%5D=three&search%5Bprivate_business%5D=private';
 
+// xvfb-run node --harmony index.js
+// pm2 start --interpreter xvfb-run npm -- start
 
 // sudo apt-get install xvfb 
 // const Xvfb = require('xvfb');
